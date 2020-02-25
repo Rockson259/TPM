677a22de37ec8863c16f89744740d03ec21e862c
Rockson259 <47891247+Rockson259@users.noreply.github.com>
2020-02-25 08:28:47 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const sql = require('mssql');
 
 const ConnectionString = require("mssql/lib/connectionstring");
